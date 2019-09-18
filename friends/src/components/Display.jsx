c2d0496b696e8668d04db85cd4e9819a48ac8bd3
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-17 17:34:53 -0700

    building form

--- friends/src/components/Display.jsx
+++ friends/src/components/Display.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import FriensList from "./";
+import Form from "./Form";
 const Display = () => {
-  return <div></div>;
+  return (
+    <div>
+      <FriendsList />
+      <h2>Create a New Friend</h2>
+      <Form />
+    </div>
+  );
 };
 
 export default Display;
--- friends/src/components/Form.jsx
+++ friends/src/components/Form.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 import { Form as Formik, Field, withFormik } from "formik";
-const Form = () => {
-  return <div></div>;
+const FriendsForm = props => {
+  return(
+  <Segment raised compact>
+    <Form>
+      <Formik>
+        <Form.Field>
+          <Field type="text" name="id" placeholder="id" />
+        </Form.Field>
+        <Form.Field>
+          <Field type="text" name="name" placeholder="name" />
+        </Form.Field>
+        <Form.Field>
+          <Field type="number" name="age" placeholder="age" />
+        </Form.Field>
+        <Form.Field>
+          <Field type="email" name="email" placeholder="email" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Formik>
+    </Form>
+  </Segment>
+  );
 };
+
+const FormikForm = withFormik({
+    mapPropsToValues(values) {
+      return {
+        id: values.id || "",
+        name: values.name || "",
+        age: values.age || "",
+        email: values.email || ""
+      };
+    },
+    handleSubmit(values, props) {
+      axios
+        .post("/api/login", values)
+        .then(res => {
+          localStorage.setItem("token", res.data.token);
+          console.log("res", res);
+        })
+        .then(res =>
+          setTimeout(() => {
+            props.props.history.push("/display");
+          }, 1000)
+        )
+        .catch(err => console.log(err));
+    }
+  })(FriendsForm);
+  export default FormikForm;
 
 export default Form;
